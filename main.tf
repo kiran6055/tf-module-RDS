be9a9d1bcc832c4f628f4847ea33ac246cf01465
kiran1616 <kirankumar7163@gmail.com>
2023-04-12 16:44:18 +0530

    updated script

--- main.tf
+++ main.tf
@@ -1,60 +1,52 @@
-#creating subnetgroup for DB
-resource "aws_elasticache_subnet_group" "default" {
-  name       = "${var.env}-elasticache-subnet-groups"
+# creating subnetgroup for RDS
+resource "aws_db_subnet_group" "default" {
+  name       = "${var.env}-rds-subnet-group"
   subnet_ids = var.subnet_ids
-
   tags = merge(
     local.common_tags,
-    { Name = "${var.env}-elasticache-subnet-groups" }
+    { Name = "${var.env}-rds-subnet-group" }
   )
 }
 
-# creating security group for DB
-resource "aws_security_group" "elasticache" {
-  name        = "${var.env}-elasticache-securitygroup"
-  description = "${var.env}-elasticache-securitygroup"
+# creating securoty group for RDS
+resource "aws_security_group" "rds" {
+  name        = "${var.env}-rds-security-group"
+  description = "${var.env}-rds-security-group"
   vpc_id      = var.vpc_id
-
   ingress {
-    description      = "elasticache"
-    from_port        = 6379
-    to_port          = 6379
-    protocol         = "tcp"
-    cidr_blocks      = var.allow_cidr
+    description = "RDS"
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = var.allow_cidr
   }
-
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
-
   tags = merge(
     local.common_tags,
-    { Name = "${var.env}-elasticache-security-groups" }
+    { Name = "${var.env}-rds-security-group" }
   )
-
 }
 
-# creating elasticache cluster
-
-resource "aws_elasticache_replication_group" "elasticache" {
-  replication_group_id       = "${var.env}-elasticache"
-  description                = "${var.env}-elasticache"
-  node_type                  = var.node_type
-  port                       = 6379
-  automatic_failover_enabled = true
-  subnet_group_name = aws_elasticache_subnet_group.default.name
-  security_group_ids = [aws_security_group.elasticache.id]
-  replicas_per_node_group = var_replicas_per_node_group
-  num_node_groups         = var.num_node_groups
+# creating RDS cluster
+resource "aws_rds_cluster" "rds" {
+  cluster_identifier     = "${var.env}-rds"
+  engine                 = var.engine
+  engine_version         = var.engine_version
+  master_username        = data.aws_ssm_parameter.DB_ADMIN_USER.value
+  master_password        = data.aws_ssm_parameter.DB_ADMIN_PASS.value
+  db_subnet_group_name   = aws_db_subnet_group.default.name
+  vpc_security_group_ids = [aws_security_group.rds.id]
+  storage_encrypted      = true
+  kms_key_id             = data.aws_kms_key.key.arn
+  #skip_final_snapshot    = true
 
   tags = merge(
     local.common_tags,
-    { Name = "${var.env}-elasticache " }
+    { Name = "${var.env}-rds" }
   )
-
-
 }
